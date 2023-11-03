d55f1273d5e9c5e1404042299e91253fe1c0e629
fajrshadiq <fajrshadiq.re@gmail.com>
2023-11-03 20:47:40 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/perjumlahan2angka/Perjumlahan2angka.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.perjumlahan2angka;
+
+/**
+ *
+ * @author Rad
+ */
+public class Perjumlahan2angka {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
